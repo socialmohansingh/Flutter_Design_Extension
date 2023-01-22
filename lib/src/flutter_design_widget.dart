b2beae4e1474b09b76756fbc502f064c151251e1
Mohan Singh Thagunna <mohan.thagunna.ext@deliveryhero.com>
2023-01-22 23:33:15 +0545

    navigator key added

--- lib/src/flutter_design_widget.dart
+++ lib/src/flutter_design_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_design_extension/flutter_design_extension.dart';
+import 'package:flutter_design_extension/src/design_tokens/colors/brands/brand_color_tokens.dart';
+import 'package:flutter_design_extension/src/design_tokens/design_token_theme_extension.dart';
 import 'package:flutter_design_extension/src/localisation/app_design_provider.dart';
 import 'package:flutter_design_extension/src/localisation/localize.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -11,12 +12,14 @@ class FlutterDesignApp extends StatefulWidget {
   final String langugePath;
   final Widget? home;
   final Brand defaultBrand;
+  final GlobalKey<NavigatorState>? navigatorKey;
 
   FlutterDesignApp({
     this.home,
     Brand? brand,
     this.langugePath = "assets/languages",
     this.languages = const [],
+    this.navigatorKey,
     super.key,
   }) : defaultBrand = (brand == null) ? DefaultBrand() : brand;
 
@@ -35,6 +38,7 @@ class _FlutterDesignAppState extends State<FlutterDesignApp> {
           final design = context.appDesign;
           return MaterialApp(
             locale: design.lang,
+            navigatorKey: widget.navigatorKey,
             localeResolutionCallback: (locale, supportedLocales) {
               if (supportedLocales.contains(locale)) {
                 return locale;
